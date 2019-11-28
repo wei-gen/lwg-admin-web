94f645f9408c82883e3ef6ea1c4502f5622e0844
weisfds <free1193984561@163.com>
2019-11-28 16:21:11 +0800

    添加页脚组件

---
+++ src/components/Footer/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './index.css';
+
+export default class Footer extends React.Component {
+
+
+
+    render() {
+
+        return (
+            <div className="footer">
+                <a href="#">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    lwg-admin
+                </a>
+            </div>
+        )
+    }
+
+}
--- src/page/Home/index.js
+++ src/page/Home/index.js
@@ -3,6 +3,7 @@ import './index.css';
 import CommonLayout from '../../layout';
 import Sider from '../../components/Sider';
 import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 export default class Home extends React.Component {
 
@@ -16,6 +17,7 @@ export default class Home extends React.Component {
                 <CommonLayout>
                     <Sider/>
                     <Header/>
+                    <Footer/>
                 </CommonLayout>
 
             </div>
